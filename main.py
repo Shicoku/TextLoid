43267c325a8d273c27c5d732ab5130730369b151
Syu <58730464+texture08@users.noreply.github.com>
2022-01-10 09:56:58 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+import tkinter
+from tkinter import font
+import time
+import win32gui
+import json
+import pyautogui
+import pyperclip
+
+root = tkinter.Tk()
+root.geometry("400x300")
+root.title(u"ボイスロイド テキスト複数生成プログラム")
+root.iconbitmap('set/icon.ico')
+
+font1 = font.Font(size=10, weight='bold')
+label = tkinter.Label(root, text="ボイスロイドに台本を読み込ませ\n1行づつ生成するソフトです", font=font1)
+label.pack(side="top")
+
+txt = tkinter.Entry(width=40)
+txt.pack(side="top")
+
+class App1(tkinter.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.save = tkinter.Button(
+            self, bg='#fd6767', fg='#ffffff', width=15, height=3)
+        self.save["text"] = "ファイルパスを保存"  # ボタンのテキスト
+        self.save["command"] = self.save_Func
+        self.save.pack(side="top")
+
+        self.make = tkinter.Button(
+            self, bg='#fd6767', fg='#ffffff', width=10, height=3)
+        self.make["text"] = "合成"  # ボタンのテキスト
+        self.make["command"] = self.make_Func  # 割り込み関数
+        self.make.pack(side="bottom")
+
+    def make_Func(self):
+        with open('set/config.json') as f:
+            config = json.load(f)
+        for window in config:
+            window = window
+        memoapp = win32gui.FindWindow(None, window)
+        time.sleep(1)
+        win32gui.SetForegroundWindow(memoapp)
+        f = open('setscript.txt', 'r', encoding='UTF-8')
+        while True:
+            data = f.readline()
+            if data == '':
+                break
+            pyperclip.copy(data.rstrip('\n'))
+            pyautogui.hotkey('ctrl', 'v')
+            save = pyautogui.locateOnScreen('img/save.png', confidence=0.9)
+            pyautogui.click(save)
+            pyautogui.hotkey('ctrl', 'v')
+            time.sleep(1)
+            pyautogui.hotkey('enter')
+            time.sleep(1)
+            pyautogui.hotkey('ctrl', 'a')
+            pyautogui.hotkey('del')
+        f.close()
+    
+    def save_Func(self):
+        config = {
+            "window": txt.get()
+        }
+        with open('config.json', 'w') as f:
+            json.dump(config, f, indent=4, ensure_ascii=False)
+
+
+app = App1(master=root)
+app.mainloop()
